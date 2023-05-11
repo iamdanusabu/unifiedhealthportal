5715835bf683c85f64843bc041436a325ec1a5a5
Danu Sabu <81710905+iamdanusabu@users.noreply.github.com>
2023-05-11 13:50:38 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,12 +1,33 @@
-const doc = document;
-const menuOpen = doc.querySelector(".menu");
-const menuClose = doc.querySelector(".close");
-const overlay = doc.querySelector(".overlay");
+const formOpenBtn = document.querySelector("#form-open");
+const home = document.querySelector(".home");
+const formContainer = document.querySelector("#form-container");
+const formCloseBtn = document.querySelector("#form-close");
+const signUpBtn = document.querySelector("#signup");
+const loginBtn = document.querySelector("#login");
+const pwShowHide = document.querySelectorAll(".pw_hide");
 
-menuOpen.addEventListener("click", () => {
-  overlay.classList.add("overlay--active");
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
 });
 
-menuClose.addEventListener("click", () => {
-  overlay.classList.remove("overlay--active");
+signUpBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
 });
