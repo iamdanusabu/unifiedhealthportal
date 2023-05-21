313cb5d8f7e86987c121f6e0183f76054a1f5383
Danu Sabu <81710905+iamdanusabu@users.noreply.github.com>
2023-05-21 23:28:37 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,33 +1,8 @@
-const formOpenBtn = document.querySelector("#form-open");
-const home = document.querySelector(".home");
-const formContainer = document.querySelector("#form-container");
-const formCloseBtn = document.querySelector("#form-close");
-const signUpBtn = document.querySelector("#signup");
-const loginBtn = document.querySelector("#login");
-const pwShowHide = document.querySelectorAll(".pw_hide");
-
-formOpenBtn.addEventListener("click", () => home.classList.add("show"));
-formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
-
-pwShowHide.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    let getPwInput = icon.parentElement.querySelector("input");
-    if (getPwInput.type === "password") {
-      getPwInput.type = "text";
-      icon.classList.replace("uil-eye-slash", "uil-eye");
-    } else {
-      getPwInput.type = "password";
-      icon.classList.replace("uil-eye", "uil-eye-slash");
-    }
-  });
-});
-
-signUpBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  formContainer.classList.add("active");
-});
-
-loginBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  formContainer.classList.remove("active");
-});
+function patientLoginPage(){
+  window.location="patientlogin.html";
+  setTimeout('patientLoginPage()',500);
+}
+function doctorLoginPage(){
+  window.location="doctorlogin.html";
+  setTimeout('patientLoginPage()',500);
+}
